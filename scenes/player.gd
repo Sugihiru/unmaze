dec1d8be466feb3bf7e014f39bf352ac9d63255b
Sugiuro <sugiuro.dev@gmail.com>
2024-10-27 16:33:08 +0100

    add sounds

--- scenes/player.gd
+++ scenes/player.gd
@@ -6,11 +6,21 @@ var jump_speed = 5
 var mouse_sensitivity = 0.002
 
 @onready var collider = $Collider
+# Footsteps
+@onready var footsteps_audio_stream_player: AudioStreamPlayer3D = $Footsteps/FootstepsAudioPlayer
+@onready var footsteps_timer: Timer = $Footsteps/FootstepsTimer
+# Ambiant sounds
+@onready var ambiant_audio_stream_player1: AudioStreamPlayer3D = $AmbiantAudios/AmbiantAudioPlayer1
+@onready var ambiant_audio_stream_player2: AudioStreamPlayer3D = $AmbiantAudios/AmbiantAudioPlayer2
+@onready var ambiant_audio_stream_player3: AudioStreamPlayer3D = $AmbiantAudios/AmbiantAudioPlayer3
+@onready var ambiant_timer: Timer = $AmbiantAudios/AmbiantTimer
+
 
 signal stairs_found
 
 func _ready():
 	collider.connect("area_entered", on_area_entered)
+	reset_ambiant_timer()
 
 func on_area_entered(area):
 	if area.is_in_group("stairs"):
@@ -21,10 +31,24 @@ func _input(event):
 		rotate_y(-event.relative.x * mouse_sensitivity)
 
 func _physics_process(delta):
-	velocity.y += -gravity * delta
 	var input = Input.get_vector("left", "right", "forward", "back")
 	var movement_dir = transform.basis * Vector3(input.x, 0, input.y)
 	velocity.x = movement_dir.x * speed
 	velocity.z = movement_dir.z * speed
 
+	if not movement_dir.is_zero_approx():
+		if footsteps_timer.time_left <= 0:
+			footsteps_audio_stream_player.pitch_scale = randf_range(0.8, 1.2)
+			footsteps_audio_stream_player.play()
+			footsteps_timer.start(0.45)
+
 	move_and_slide()
+
+
+func reset_ambiant_timer():
+	ambiant_timer.start(randi_range(30, 40))
+
+
+func _on_ambiant_timer_timeout():
+	var audio_players = [ambiant_audio_stream_player1, ambiant_audio_stream_player2, ambiant_audio_stream_player3]
+	audio_players.pick_random().play()
